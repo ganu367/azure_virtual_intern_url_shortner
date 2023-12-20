bde512ed40cb57ab9c927751c4fd2cd50b0e9bc2
Ganu367 <ganeshmane1184@gmail.com>
2023-12-20 21:17:31 +0530

    update code

--- database.py
+++ database.py
@@ -1,12 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dotenv import dotenv_values, load_dotenv
+# from dotenv import dotenv_values, load_dotenv
 import os
 import psycopg2
 
-config = dotenv_values(".env")
-connect = load_dotenv()
+# config = dotenv_values(".env")
+# connect = load_dotenv()
 
 # defining postgres sql database
 # SQLALCHEMY_DATABASE_URL = os.getenv('SQLITE_URL')
