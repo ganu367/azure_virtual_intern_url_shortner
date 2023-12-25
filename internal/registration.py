55b4419bd4a026139b7dfd892b49313e9c4d211d
Ganesh Raghunath Mane <89238495+ganu367@users.noreply.github.com>
2023-12-25 19:45:49 +0530

    Update registration.py

--- internal/registration.py
+++ internal/registration.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response, Request
-from fastapi.responses import RedirectResponse, HTMLResponse
+from fastapi.responses import RedirectResponse, HTMLResponse 
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
